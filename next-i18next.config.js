9909b9759b70566af501c192464c40b2f8b5bd28
Zachery Liu <Zachery_Liu@Outlook.com>
2023-06-18 16:53:52 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'zh-CN',
-    locales: ['de-DE', 'en', 'es', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-HK']
+    locales: ['de-DE', 'en', 'es', 'zh-CN', 'hi', 'tr-TR', 'zh-HK']
   },
   localePath: path.resolve('public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
